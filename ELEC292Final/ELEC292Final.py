6031561293f449daa20fd13f67eeb4b3eb07eff0
lizzyvroom15 <148002880+lizzyvroom15@users.noreply.github.com>
2024-03-26 13:08:21 -0400

    Update ELEC292Final.py

--- ELEC292Final/ELEC292Final.py
+++ ELEC292Final/ELEC292Final.py
@@ -51,11 +51,13 @@ with h5py.File("data.h5", 'w') as hdf_file:
 #plot x, y and z vs time for each data type
 # 6 plots per person
 
+
+#WALKING X PLOTS
 #dj data walking x plot
-plt.firgure(figszie = (10, 8))
+plt.figure(figsize = (10, 8))
 #by default plot draws lines between the pointss/connects the data
 dj_data_walking.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
-plt.title('Acceleration in x')
+plt.title('DJ Acceleration in x')
 #adding labels to the axis
 plt.xlabel("Time (s)")
 plt.ylabel("Linear Acceleration x (m/s^2)")
@@ -68,6 +70,300 @@ plt.tight_layout()
 #function to show the plot
 plt.show()
 
+#lizzy data walking x plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_walking.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
+plt.title('Lizzy Acceleration in x')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration x (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["X acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data walking x plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_walking.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
+plt.title('Isabel Acceleration in x')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration x (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["X acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#WALKING Y PLOTS
+#dj data walking y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+dj_data_walking.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('DJ Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#lizzy data walking y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_walking.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Lizzy Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data walking y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_walking.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Isabel Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#WALKING Z PLOTS
+#dj data walking z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+dj_data_walking.plot(x='Time (s)', y='Linear Acceleration z (m/s^2)')
+plt.title('DJ Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#lizzy data walking z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_walking.plot(x='Time (s)', y='Linear Acceleration z (m/s^2)')
+plt.title('Lizzy Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data walking z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_walking.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Isabel Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#JUMPING PLOTS
+#JUMPING X PLOTS
+#dj data jumping x plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+dj_data_jumping.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
+plt.title('DJ jumping Acceleration in x')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration x (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["X acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#lizzy data jumping x plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_jumping.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
+plt.title('Lizzy jumping Acceleration in x')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration x (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["X acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data jumping x plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_jumping.plot(x='Time (s)', y='Linear Acceleration x (m/s^2)')
+plt.title('Isabel jumping Acceleration in x')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration x (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["X acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#JUMPING Y PLOTS
+#dj data jumping y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+dj_data_jumping.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('DJ jumping Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#lizzy data jumping y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_jumping.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Lizzy jumping Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data jumping y plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_jumping.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Isabel Acceleration in y')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration y (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Y acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#JUMPING Z PLOTS
+#dj data jumping z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+dj_data_jumping.plot(x='Time (s)', y='Linear Acceleration z (m/s^2)')
+plt.title('DJ jumping Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#lizzy data jumping z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+lizzy_data_jumping.plot(x='Time (s)', y='Linear Acceleration z (m/s^2)')
+plt.title('Lizzy jumping Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
+
+#isabel data jumping z plot
+plt.figure(figsize = (10, 8))
+#by default plot draws lines between the pointss/connects the data
+isabel_data_walking.plot(x='Time (s)', y='Linear Acceleration y (m/s^2)')
+plt.title('Isabel jumping Acceleration in z')
+#adding labels to the axis
+plt.xlabel("Time (s)")
+plt.ylabel("Linear Acceleration z (m/s^2)")
+#rotatess the labels 90 degrees to make it look better/no overlap
+pl.xticks(rotation = 90)
+#adding a legend
+plt.legend(["Z acceleration"])
+#formts the plot (fixes distance between elements and labels)
+plt.tight_layout()
+#function to show the plot
+plt.show()
 
 # Graphs Data like from lab 5
 window_size = [5]
