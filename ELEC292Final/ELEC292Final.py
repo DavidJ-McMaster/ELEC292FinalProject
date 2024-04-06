5c7ca10470e1b991c37ccd385664e997efcd13e4
David (DJ) McMaster <21djmm@queensu.ca>
2024-04-06 13:53:10 -0400

    DataStuffs

--- ELEC292Final/ELEC292Final.py
+++ ELEC292Final/ELEC292Final.py
@@ -27,14 +27,18 @@ def segmentation(signal,
 dj_walking = "dj_WalkingRawData.csv"
 dj_jumping = "dj_JumpingRawData.csv"
 
-isabel_walking = ["Isabel_Walking_RawData.csv", "Isabel_Walking_RawData2.csv"]
+isabel_walking = ["Isabel_Walking_RawData.csv", 
+                  "Isabel_Walking_RawData2.csv" ]
 isabel_w_dataframe = [pd.read_csv(file) for file in isabel_walking]
 
-isabel_jumping = ["Isabel_Jumping_RawData.csv", "Isabel_Jumping_RawData_2.csv", "Isabel_Jumping_RawData_3.csv", "Isabel_Jumping_RawData4.csv"]
+isabel_jumping = ["Isabel_Jumping_RawData.csv", 
+                  "Isabel_Jumping_RawData_2.csv", 
+                  "Isabel_Jumping_RawData_3.csv", 
+                  "Isabel_Jumping_RawData4.csv" ]
 isabel_j_dataframe = [pd.read_csv(file) for file in isabel_jumping]
 
-lizzy_walking = "lizzy_WalkingRawData.csv"
-lizzy_jumping = "lizzy_jumpingRawData.csv"
+lizzy_walking = "lizzy_NewWalkingData.csv"
+lizzy_jumping = "lizzy_NewJumpingData.csv"
 
 
 # reads the CSV files into data frames
